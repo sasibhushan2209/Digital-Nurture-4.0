aaa14cd2755080ef629cc7b3ebd7463309da550c
sasibhushan2209 <sasibhushan2209@gmail.com>
2025-07-25 22:14:09 +0530

    week 6 folder added

---
+++ week6_Exercises/3. ReactJS-HOL/scorecalculatorapp/src/Components/CalculateScore.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../Stylesheets/mystyle.css';
+
+function CalculateScore(props) {
+  const average = props.total / props.goal;
+
+  return (
+    <div className="container">
+      <h2>Student Score Calculator</h2>
+      <p><strong>Name:</strong> {props.name}</p>
+      <p><strong>School:</strong> {props.school}</p>
+      <p><strong>Total Marks:</strong> {props.total}</p>
+      <p><strong>Goal:</strong> {props.goal}</p>
+      <p className="result"><strong>Average Score:</strong> {average.toFixed(2)}</p>
+    </div>
+  );
+}
+
+export default CalculateScore;
