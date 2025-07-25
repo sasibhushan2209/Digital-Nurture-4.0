cc1a1f55c18bce2dfd00c2cb014ec943a981385b
sasibhushan2209 <sasibhushan2209@gmail.com>
2025-07-25 22:19:56 +0530

    added some more

---
+++ Cognizant/DigitalNurture/week6_Exercises/3. ReactJS-HOL/scorecalculatorapp/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CalculateScore from './Components/CalculateScore';
+
+function App() {
+  return (
+    <div>
+      <CalculateScore name="P V Sasibhushan Raju" school="Narayana EM High School" total={599} goal={6} Percentage = "99.9%" />
+    </div>
+  );
+}
+
+export default App;
